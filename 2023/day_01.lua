1d6d8f6acb5c48e6bc30f3261f94b8ea6744eb36
Julio Ernesto Rodríguez Cabañas <julioernesto90@gmail.com>
2023-12-02 00:21:51 +0000

    Solve problem 1 part 2

--- 2023/day_01.lua
+++ 2023/day_01.lua
@@ -18,7 +18,34 @@ function part_1(lines)
 end
 
 function part_2(lines)
-  return 0
+  local sum = 0
+  local digitsMap = {
+    ["1"] = "1", ["2"] = "2", ["3"] = "3", ["4"] = "4", ["5"] = "5", ["6"] = "6", ["7"] = "7", ["8"] = "8", ["9"] = "9", ["0"] = "0",
+      one = "1",   two = "2", three = "3",  four = "4",  five = "5",   six = "6", seven = "7", eight = "8",  nine = "9",
+  }
+
+  for _, line in ipairs(lines) do
+    local firstDigit, lastDigit
+
+    for digitWord, digitValue in pairs(digitsMap) do
+      local findStart = 1
+      while findStart <= #line do
+        local digitStart, digitEnd = line:find(digitWord, findStart)
+        if digitStart == nil then
+          break
+        else
+          if firstDigit == nil or digitStart < firstDigit.digitStart then firstDigit = { digitStart = digitStart, digit = digitValue } end
+          if lastDigit == nil or digitEnd > lastDigit.digitEnd then lastDigit = { digitEnd = digitEnd, digit = digitValue } end
+          findStart = findStart + 1
+        end
+      end
+    end
+
+    local number = tonumber(firstDigit.digit .. lastDigit.digit)
+    sum = sum + number
+  end
+
+  return sum
 end
 
 local lines = read_lines(problemNumber)
